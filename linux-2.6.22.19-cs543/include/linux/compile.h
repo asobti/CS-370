3444eaad45fd544fd882ca78abea816cb85487d7
Ayush Sobti <as948@drexel.edu>
2013-03-12 21:04:02 -0400

    removed o files

--- linux-2.6.22.19-cs543/include/linux/compile.h
+++
@@ -1,8 +0,0 @@
-/* This file is auto generated, version 6 */
-#define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#6 Tue Mar 12 20:48:34 EDT 2013"
-#define LINUX_COMPILE_TIME "20:48:34"
-#define LINUX_COMPILE_BY "as948"
-#define LINUX_COMPILE_HOST "ws31.cs.drexel.edu"
-#define LINUX_COMPILE_DOMAIN "cs.drexel.edu"
-#define LINUX_COMPILER "gcc version 4.6.3 (Ubuntu/Linaro 4.6.3-1ubuntu5) "
